75c7199bf73dd844ecb063a80c824094eb61635a
Jim kp <jimkapadoukas@gmail.com>
2022-04-03 19:02:28 +0300

    Added Exception handling

---
+++ src/main/java/dating/dating/exceptions/GlobalExceptionsHandling.java
@@ -0,0 +1,10 @@
+package dating.dating.exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionsHandling 
+{
+    int code;
+    String message;
+}
